c7588dedb168897725d147d0d88d601c995d5f11
skrstv123 <skrstv123@gmail.com>
2019-12-20 03:46:22 +0530

    updated helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,12 @@
 from flask import Flask as fl
 
 app = fl(__name__) 
+#About the First Parameter 
+#The idea of the first parameter __name__
+# is to give Flask an idea of what belongs to your application. This name is used to find resources on the filesystem, can be used by extensions to improve debugging information and a lot more.
+#so basically we're telling flask that all the resources (templates, static files, etc) are placed in the same directory which contains this python file
+
+
 boot = """<!-- Latest compiled and minified CSS -->
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
 
@@ -10,10 +16,6 @@ boot = """<!-- Latest compiled and minified CSS -->
 <!-- Latest compiled and minified JavaScript -->
 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
 	"""
-#About the First Parameter 
-#The idea of the first parameter __name__
-# is to give Flask an idea of what belongs to your application. This name is used to find resources on the filesystem, can be used by extensions to improve debugging information and a lot more.
-#so basically we're telling flask that all the resources (templates, static files, etc) are placed in the same directory which contains this python file
 
 @app.route('/')
 def index():
