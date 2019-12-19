54e22e42f2abd633143fa694defda28970effa99
skrstv123 <skrstv123@gmail.com>
2019-12-19 17:35:00 +0530

    basics

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+from flask import Flask as fl
+
+app = fl(__name__) 
+#About the First Parameter 
+#The idea of the first parameter __name__
+# is to give Flask an idea of what belongs to your application. This name is used to find resources on the filesystem, can be used by extensions to improve debugging information and a lot more.
+
+@app.route('/')
+def index():
+	html="""<!-- Latest compiled and minified CSS -->
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+
+<!-- Optional theme -->
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
+<div class='jumbotron' align='center'><h1>Hello World</h1></div>
+	"""
+	return html 
+	
+if __name__=='__main__':
+	app.run() 
+
